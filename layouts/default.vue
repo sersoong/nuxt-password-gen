42bd1da831859798263ece379c448b59be40930a
sersoong <sersoong@gmail.com>
2018-12-30 17:30:13 +0800

    删除layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,18 @@
 <template>
   <v-app>
-    <v-layout align-center justify-space-around>
-      <v-toolbar app>
-        <v-toolbar-title class="headline text-uppercase">
-          <span>密码生成器</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn 
-        icon
-        href="https://github.com/sersoong/nuxt-password-gen"
-        target="_blank"
-        >
-          <v-icon>fab fa-github</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </v-layout>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>密码生成器</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn 
+      icon
+      href="https://github.com/sersoong/nuxt-password-gen"
+      target="_blank"
+      >
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+    </v-toolbar>
     
     <v-content>
       <v-container>
